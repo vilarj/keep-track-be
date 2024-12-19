398778d382f5d9fc4c08dfc0c822756367aae67d
vilarj <lamaravillasan@outlook.com>
2024-12-19 03:22:47 -0400

    Minor update on launchSettings.json | Adding the db conn string

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,28 +1,27 @@
 using keep_track_be.Models;
 using keep_track_be.Services;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace keep_track_be.Controllers;
 
 [ApiController]
-[Route("[controller]/user")]
-public class UserController(ILogger<UserController> logger, UserService userService)
+[Route("api/[controller]")]
+public class UserController(ILogger<UserController> logger, UserService userService) : ControllerBase
 {
 
     [HttpGet("{id:long}")]
     public async Task<ActionResult<User>> GetUserByIdAsync(long id)
     {
-        // TODO: Need to implement this
-        // var user = await userService.GetUserByIdAsync(id);
-        //
-        // if (user == null)
-        // {
-        //     return NotFound(new { Message = $"User with ID {id} was not found." });
-        // }
-        //
-        // logger.LogInformation("Get user by id: {id}", id);
-        // return Ok(user);
-        return null;
+        logger.LogInformation("Fetching user by ID: {id}", id);
+
+        var user = await userService.GetUserByIdAsync(id);
+
+        if (user == null)
+        {
+            logger.LogWarning("User with ID: {id} not found.", id);
+            return NotFound(new { Message = $"User with ID {id} not found." });
+        }
+
+        return Ok(user);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using keep_track_be.Context;
 using keep_track_be.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<KeepTrackContext>(options => {}); // TODO: Need to connect to Postgresql
+builder.Services.AddDbContext<KeepTrackContext>(options => options.UseNpgsql(builder.Configuration["connString"]));
 builder.Services.AddControllers();
 builder.Services.AddScoped<UserService>();
 
